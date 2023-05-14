832f4a1f5759ebc703293af39c7fdef1e6a20110
tensai <tensai@gmail.com>
2023-05-14 18:32:53 +0530

    improved crudapp home component

--- frontend/src/components/CrudApp.jsx
+++ frontend/src/components/CrudApp.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import { styled } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  styled,
+} from '@mui/material';
 
-const MyComponent = styled('div')({
-  color: 'darkslategray',
-  backgroundColor: 'aliceblue',
-  fontSize: '20px',
-  padding: 10,
-  paddingLeft: '50px',
-  borderRadius: 4,
+const StyledCard = styled(Card)({
+  margin: 'auto',
+  marginTop: '50px',
 });
+
+const styles = {
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    marginTop: '30',
+  },
+};
 
 const CrudApp = () => {
   return (
-    <>
-      <MyComponent>Hello from CrudApp</MyComponent>
-    </>
+    <StyledCard sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={require('../assets/CRUD.jpeg')}
+        title="CRUD"
+        style={styles.media}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          CRUD Application
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          CRUD is an acronym that comes from the world of computer programming
+          and refers to the four functions that are considered necessary to
+          implement a persistent storage application: create, read, update and
+          delete.
+        </Typography>
+      </CardContent>
+    </StyledCard>
   );
 };
 
